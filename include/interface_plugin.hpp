4b03a98c29a24c72ddb5f8e7be601dd2a87e76d1
Tobias Møller Jensen <29657183+tobias1012@users.noreply.github.com>
2024-09-25 08:45:01 +0200

    initial structure

---
+++ include/interface_plugin.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <hex/plugin.hpp>
+#include <hex/providers/provider.hpp>
+
+class InterfaceProvider : public hex : prv : Provider
+{
+public:
+    InterfaceProvider : hex::prv::Provider(){};
+
+}
--- source/interface_plugin.cpp
+++ source/interface_plugin.cpp
@@ -1,4 +1,5 @@
-#include <hex/plugin.hpp>
+#include "interface_plugin.hpp"
+#include <hex/api/content_registry.hpp>
 
 // Browse through the headers in lib/libimhex/include/hex/api/ to see what you can do with the API.
 // Most important ones are <hex/api/imhex_api.hpp> and <hex/api/content_registry.hpp>
@@ -6,6 +7,6 @@
 // This is the main entry point of your plugin. The code in the body of this construct will be executed
 // when ImHex starts up and loads the plugin.
 // The strings in the header are used to display information about the plugin in the UI.
-IMHEX_PLUGIN_SETUP("Example Plugin", "Author", "Description") {
-    // Put your init code here
+IMHEX_PLUGIN_SETUP("Interface Provider", "tobias1012", "Plugin for reading data from interfaces") {
+    hex::ContentRegistry::Provider::add<InterfaceProvider>(true);
 }
