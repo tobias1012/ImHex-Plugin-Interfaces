08de0b5668f2ee99180be23e974077ee0e304c26
Tobias Møller Jensen <29657183+tobias1012@users.noreply.github.com>
2024-09-25 17:59:36 +0200

    update build

--- include/interface_plugin.hpp
+++ include/interface_plugin.hpp
@@ -1,4 +1,8 @@
 #pragma once
+#if defined(OS_WINDOWS)
+    #include <windows.h>
+#endif
+
 
 #include <hex/plugin.hpp>
 #include <hex/providers/provider.hpp>
@@ -7,5 +11,13 @@ class InterfaceProvider : public hex : prv : Provider
 {
 public:
     InterfaceProvider : hex::prv::Provider(){};
+    ~InterfaceProvider() override = default;
+
+    [[nodiscard]] bool isReadable() const override { return true; }
+    [[nodiscard]] bool isWritable() const override { return false; }
+    [[nodiscard]] bool isResizable() const override { return false; }
+    [[nodiscard]] bool isSavable() const override { return false; }
+    [[nodiscard]] bool isDumpable() const override { return false; }
+
 
 }
