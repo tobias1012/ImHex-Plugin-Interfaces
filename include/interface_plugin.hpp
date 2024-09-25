f3911f830727f5601c3aa9eecb6172763f0b3b96
Tobias Møller Jensen <29657183+tobias1012@users.noreply.github.com>
2024-09-25 18:21:32 +0200

    Adding more standard functions

--- include/interface_plugin.hpp
+++ include/interface_plugin.hpp
@@ -6,18 +6,26 @@
 
 #include <hex/plugin.hpp>
 #include <hex/providers/provider.hpp>
+#include <hex/helpers/intrinsics.hpp> // Unused function
 
-class InterfaceProvider : public hex : prv : Provider
+class InterfaceProvider : public hex:prv:Provider
 {
 public:
     InterfaceProvider : hex::prv::Provider(){};
     ~InterfaceProvider() override = default;
 
+    [[nodiscard]] bool isAvailable() const override {return true;}
     [[nodiscard]] bool isReadable() const override { return true; }
     [[nodiscard]] bool isWritable() const override { return false; }
     [[nodiscard]] bool isResizable() const override { return false; }
     [[nodiscard]] bool isSavable() const override { return false; }
     [[nodiscard]] bool isDumpable() const override { return false; }
 
+    void readRaw(u64 offset, void *buffer, size_t size) override {}; //This will need a function later
+    void writeRaw(u64 offset, const void *buffer, size_t size) override { hex::unused(offset, buffer, size);}
+    [[nodiscard]] u64 getActualSize() const override {return 0xFFFF;} // This should be updated
+
+    [[nodiscard]] std::string getName() const override;
+    [[nodiscard]] std::vector<Description> getDataDescription() const override { return { }; };
 
 }
